865164a0d2869709da2c78fbb587d01dfde9b0ff
Neil Yanga <neilyanga.ucsb@gmail.com>
2022-04-04 21:49:52 -0700

    Added messages form and list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Lesson 4.2
 let today = new Date();
 let thisYear = today.getFullYear();
 let footer = document.querySelector('footer');
@@ -6,7 +7,7 @@ let skills = ['Javascript', 'HTML', 'Excel', 'PPT', 'Word'];
 let skillsSection = document.querySelector('#skills');
 let skillsList = skillsSection.querySelector('ul');
 
-copyright.innerHTML = `Neil Yanga ${thisYear}`;
+copyright.innerHTML = `&copy Neil Yanga ${thisYear}`;
 
 footer.appendChild(copyright);
 
@@ -15,4 +16,39 @@ for (let value of skills){
     skill.innerHTML = value;
     skillsList.appendChild(skill);
 }
+
+//Lesson 4.3
+let messageForm = document.querySelector("form[name='leave_message']");
+messageForm.addEventListener('submit',(event)=> {
+    event.preventDefault();
+    let name = event.target.name.value;
+    let email = event.target.email.value;
+    let message = event.target.message.value;
+    console.log(`${name} ${email} ${message}`);
+
+    //Takes the user input message displayed in a list
+    let messageSection = document.querySelector('#messages');
+    let messageList = messageSection.querySelector('ul');
+    let newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `<a href="mailto:email">${name}</a> wrote: <span>${message}</span>  `;
+
+    //Creates remove button for each list item
+    let removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', (event) => {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    })
+
+
+    //Appends these elements into the right parent node.
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    //resets form input after you hit submit
+    messageForm.reset();
+});
 
